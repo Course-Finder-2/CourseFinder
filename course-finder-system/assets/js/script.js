41b18896bf00552171edc298ad5ce2b0a23261d9
Azziiieee <balendez.alodiazenny26@gmail.com>
2026-05-11 20:57:30 +0800

    format and fixed layout style

--- course-finder-system/assets/js/script.js
+++ course-finder-system/assets/js/script.js
@@ -1,6 +1,5 @@
 console.log("Course Finder System Loaded");
 
-// Simple confirmation for delete actions
 function confirmDelete() {
     return confirm("Are you sure you want to delete this record?");
 }
